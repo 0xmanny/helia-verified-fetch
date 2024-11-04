4fd9c02029ff27f9529b691c2eb26ab71d8178bc
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-11-04 15:24:07 -0600

    chore: adjust dir listing conformance test

--- packages/gateway-conformance/src/conformance.spec.ts
+++ packages/gateway-conformance/src/conformance.spec.ts
@@ -241,7 +241,7 @@ const tests: TestConfig[] = [
       'TestUnixFSDirectoryListingOnSubdomainGateway'
       // 'TestUnixFSDirectoryListing/.*TODO:_cleanup_Kubo-specifics'
     ],
-    successRate: 50
+    successRate: 27.27
   },
   {
     name: 'TestTar',
